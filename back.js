f26f17658133b34faa1a7eef78d74225be5e0d3d
Luke Caish-Sadlek <64913461+Luke-Calek@users.noreply.github.com>
2023-12-23 21:41:22 +0100

    v0.1

---
+++ back.js
@@ -0,0 +1,18 @@
+
+// KEYBOARD PRESSES
+
+// Event listener for key presses
+document.addEventListener("keydown", function (event) {
+  // Check if the pressed key is the 'Minus' key
+  if (event.key === 'b') {
+    // Navigate to preview page
+    history.back()
+  }
+});
+
+
+
+
+
+
+
